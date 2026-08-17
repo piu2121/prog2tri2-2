d7937f5b3bb6ddd70d0897a069de384266a436e5
piu2121 <elias71802@gmail.com>
2026-08-17 18:38:59 +0000

    feito funcional

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # prog2tri2-2
 segunda nota com Rodrigo
 - busca binaria
+
+um algoritmo simples de busca binaria
--- busca_binaria.js
+++ busca_binaria.js
@@ -1,29 +1,35 @@
+let contador=0
 const Logica_binaria = (vetor, ponteiroMaior, ponteiroMenor, valor) => {
     const metade_posicao = Math.floor((ponteiroMaior + ponteiroMenor) / 2)
-    console.log(vetor)
     console.log('menor', ponteiroMenor)
     console.log('valor', valor)
     console.log('maior', ponteiroMaior)
     console.log('posicao atual', metade_posicao)
     if (vetor.length % 2 === 0) {
         if (vetor[metade_posicao] === valor) {
-            return vetor[metade_posicao]
+            return console.log('valor escolhido',vetor[metade_posicao],'contador',contador+1)
         }
         else if (vetor[metade_posicao] > valor) {
-            Logica_binaria(vetor, ponteiroMaior, metade_posicao, valor)
+            contador++
+            Logica_binaria(vetor, metade_posicao, ponteiroMenor, valor)
         }
         else {
-            Logica_binaria(vetor, metade_posicao, ponteiroMenor, valor)
+            contador++
+            Logica_binaria(vetor, ponteiroMaior, metade_posicao, valor)
         }
     } else {
         const metade_posicao = Math.floor((ponteiroMaior + ponteiroMenor) / 2)
         if (vetor[metade_posicao] === valor) {
-            return vetor[metade_posicao]
+            return console.log('valor escolhido',vetor[metade_posicao],'contador',contador+1)
         }
         else if (vetor[metade_posicao] > valor) {
+            Logica_binaria(vetor, metade_posicao, ponteiroMenor, valor)
+        contador++
+        }
+        else {
+            contador++
             Logica_binaria(vetor, ponteiroMaior, metade_posicao, valor)
         }
-        else { Logica_binaria(vetor, metade_posicao, ponteiroMenor, valor) }
     }
 }
 const buscabinaria_nao_encadeada = async (vetor, valor) => {
@@ -32,7 +38,15 @@ const buscabinaria_nao_encadeada = async (vetor, valor) => {
     } else {
         let ponteiroMenor = 0
         let ponteiroMaior = vetor.length - 1
-        return await Logica_binaria(vetor, ponteiroMaior, ponteiroMenor, valor)
+        if (valor === vetor[ponteiroMenor]) {
+            return console.log(vetor[ponteiroMenor])
+        }
+        else if (valor === vetor[ponteiroMaior]) {
+            return console.log(vetor[ponteiroMaior])
+        }
+        else {
+            return await Logica_binaria(vetor, ponteiroMaior, ponteiroMenor, valor)
+        }
     }
 }
-buscabinaria_nao_encadeada([1, 2, 3, 4], 2)
+buscabinaria_nao_encadeada( Array.from({ length: 1000000 }, (_, i) => i),4)
